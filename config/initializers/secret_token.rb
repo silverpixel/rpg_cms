6d880d5d4a8bf2b1f0d1aa895b5ce6f9dac98712
silverpixel <enferty@Nastasjas-Mac.local>
2015-05-06 10:03:05 +0200

    v.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RpgCms::Application.config.secret_key_base = 'fdcfa03ae58573bd5b06b2dad7c37859fcaf2ba0cbbe02e7352ba79e79de0136f803a437cfef2ad92b6176f50d8d3acc83293d865b932392bc260d622275ac8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RpgCms::Application.config.session_store :cookie_store, key: '_rpg_cms_session'
